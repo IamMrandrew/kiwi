7081c3a08b44c5b5e2f23a15572b835c0061cddd
iammrandrew <andrewliyanlap000824@gmail.com>
2022-08-20 00:20:11 +0800

    Starter code for CoreData and CloudKit with MVVM

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,25 +8,81 @@
 import SwiftUI
 import CoreData
 
+import Combine
+
+class ContentViewModel: ObservableObject {
+    @Published var items: [Item] = []
+    
+    private let viewContext = PersistenceController.shared.container.viewContext
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        fetchAllTimeItems()
+        PersistenceController.shared.$cloudEvent
+            .sink{ newValue in
+                print("PersistenceController cloudEvent property changed")
+                self.fetchAllTimeItems()
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func fetchAllTimeItems() {
+        do {
+            let timeDataRequest: NSFetchRequest<Item> = Item.fetchRequest()
+            items = try viewContext.fetch(timeDataRequest)
+        } catch {
+           NSLog("Handle Error!")
+        }
+    }
+    
+    func addItem() {
+//        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+
+            do {
+                try viewContext.save()
+//                fetchAllTimeItems()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+//        }
+    }
+
+    func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+//                fetchAllTimeItems()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+//        }
+    }
+}
+
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @StateObject var vm = ContentViewModel()
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
+                ForEach(vm.items) { item in
                     NavigationLink {
                         Text("Item at \(item.timestamp!, formatter: itemFormatter)")
                     } label: {
                         Text(item.timestamp!, formatter: itemFormatter)
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .onDelete(perform: vm.deleteItems)
             }
             .toolbar {
 #if os(iOS)
@@ -35,43 +91,12 @@ struct ContentView: View {
                 }
 #endif
                 ToolbarItem {
-                    Button(action: addItem) {
+                    Button(action: vm.addItem) {
                         Label("Add Item", systemImage: "plus")
                     }
                 }
             }
             Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
         }
     }
 }
@@ -85,6 +110,6 @@ private let itemFormatter: DateFormatter = {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
